7be2cf8b5961ea57c34cbf3ae2cb7de632026390
Avsavi <shingavinash7@gmail.com>
2025-10-29 22:39:58 +0530

    code changes

--- css practice/index.js
+++ css practice/index.js
@@ -15,7 +15,7 @@ btn.addEventListener("click",(e)=>{
 });
 const btn1= document.querySelector(".btn1");
 btn1.style.background="blue";
-btn1.style.color="white";  */
+btn1.style.color="white";  
 
 const myForm =document.querySelector('.container');
 const nameInput =document.querySelector('.studentname');
@@ -35,4 +35,68 @@ function onSubmit(e) {
     }
  
     
+}*/
+//------------------if else statement ----------------------------//
+
+/*let age = 21;
+if (age<=5) {
+    console.log("he is child");
 }
+    else if( age<=18){
+console.log("He is minor")
+    }
+    else if(age>=60){
+        console.log("He is an seniour citizen")
+    }
+ else {
+    console.log("He is adult")
+}*/
+//------------------------------------switch case break----------------------------------//
+let age = 21;
+switch (age) {
+    case 18: console.log("he is minor")
+        break;
+    case 21: console.log("He is adult ")
+        break;
+
+    case 65: console.log("He is an seniour citizen")
+        break;
+    default: console.log("He is child");
+}
+
+//------------------------------------ for loops-----------------------------------------//
+
+/*let i;
+
+for (i = 1; i <= 5; i++) {
+    console.log("*");
+}*/
+
+
+//------------------ do loops------------------------------------------//
+ /*i=1;
+ while (i<=10){
+    console.log("*");
+    i++
+ }*/
+
+ //------------------------dowhile loop-------------------------------//
+ /*i=1;
+ do {
+    console.log("&");
+    i++
+ } while (i<=8);*/
+
+ //-------------------------------prog----------------------------//
+
+ let i;
+ let j;
+ let row;
+ let n =5;
+ for(i=1;i<=n;i++){
+    row="";
+    for(j=1;j<=n;j++){
+        row=row +"*";
+    }
+    console.log(row);
+ }
