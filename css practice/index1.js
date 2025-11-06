f447d4ddb798f4800aa3453c42a563826636b4e0
Avsavi <shingavinash7@gmail.com>
2025-11-06 11:45:58 +0530

    code changes

---
+++ css practice/index1.js
@@ -0,0 +1,50 @@
+// calculate student percentage 
+/*
+function calGrade(marks, totalmarks) {
+  
+  let percentage = (marks / totalmarks) * 100;
+  let grade;
+
+    if (marks >= 90) {
+        grade = 'A';
+    }
+    else if (marks >= 80) {
+        grade = 'B';
+    }
+    else if (marks >= 70) {
+        grade = 'C';
+    }
+    else if (marks >= 60) {
+        grade = 'D';
+    }
+    else if (marks <=35) {    
+        grade = 'F';
+    }
+    else{
+        grade = 'Fail';
+    }
+    console.log(grade); 
+}  
+calGrade(60,100);
+calGrade(50,100);
+calGrade(70,100);
+calGrade(10,40);
+calGrade(100,100);
+calGrade(80,100); 
+=============================
+function num (totalnumber)
+{
+ for (let i = 0; i <= totalnumber; i++) {
+console.log("number of i ="+ i);
+ }
+}
+num(6); */
+
+function num(totalnumber) {
+    for (let i = 0; i <= totalnumber; i++) {
+        console.log("number of i = " + i);
+    }
+}
+
+// Call the function
+num(6);
