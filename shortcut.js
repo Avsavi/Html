31cf963519de6ce190dc34285efdefecb6274ed6
Avsavi <shingavinash7@gmail.com>
2025-11-14 19:03:48 +0530

    code changes

---
+++ shortcut.js
@@ -0,0 +1,53 @@
+/*
+let age =10 ;
+let  isAdult = age >= 18 ? "yes" : "no";  // Ternary Operator 
+
+console.log(isAdult);
+
+
+
+const isAge = age=> age >= 18 ? "Yes" : "No";
+
+console.log(isAge(32)); // "Yes"
+
+
+
+let sum = 13;
+
+let cal = sum % 2 ===0 ? "even" : "odd"
+
+console.log(cal);
+
+
+let mul = 22;
+
+let finalCal = (mul + 2) % 3 ===0 ? "even": "odd"
+
+console.log(finalCal);
+
+
+let mul = 14;
+let add = mul +2
+let finalCal = add %2 ===0 ? "even": "odd"
+
+console.log(finalCal);
+*/
+
+var a="29";
+var b=49;
+var c=50;
+
+var d="My age is"+a+" my mark is"+b+'lmy addre no'+c;
+var e=`my age is ${a} 
+my mark is ${b}
+ my adresss no is ${c}`;
+
+ console.log(e);
+ 
+
+
+
+
+ 
+
+  
