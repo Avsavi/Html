515c11685f57d80fc0dabddbd60def33469833dd
Avsavi <shingavinash7@gmail.com>
2025-11-28 16:13:13 +0530

    code

---
+++ a&o1.js
@@ -0,0 +1,14 @@
+// object and aaray separate method
+
+let num = [10,20,
+    {nam:'dev', age:24, adult: 'yes'},
+    ["ram","laxman","hanuman"],
+    {age:28, add:"xyz",conatct:987889888}
+]
+
+let result=num[4].conatct;
+console.log(result);
+
+
+let result1 =num[3][2];
+console.log(result1);
