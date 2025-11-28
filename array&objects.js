8a3f2826d332d658f6533b6a5c26d2d5d9a7d6cf
Avsavi <shingavinash7@gmail.com>
2025-11-28 16:09:03 +0530

    done

---
+++ array&objects.js
@@ -0,0 +1,19 @@
+
+
+// practice set of aaaray and objects 
+
+let fruits = ["apple","banana","chiku",["sitaphal","awla",["khira","kharwas","sweetpotato"]]];
+
+let result = fruits[0];
+console.log(result);
+
+let mixresult =fruits[3][1];
+console.log(mixresult);
+
+let result1= fruits[3][2][0];
+console.log(result1);
+
+
+
+
+
