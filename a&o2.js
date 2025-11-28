dbb39201c2083e81f2d104ea75e3f40def7cd699
Avsavi <shingavinash7@gmail.com>
2025-11-28 16:26:59 +0530

    object mix

---
+++ a&o2.js
@@ -0,0 +1,22 @@
+
+let ename={ ename1: "rahul", ecode:123, eadd:"123garwarecollge",
+    marks:[60,70,90,100],
+    year:2009,
+    grade:["a","b","c"]
+
+}
+let eresult=ename.ecode;
+console.log(eresult);
+
+
+let result=ename.grade[2];
+console.log(result);
+
+
+let r2=ename.ename1;
+console.log(r2);
+
+let r3 =ename.year;
+console.log(r3);
+
+
