505fb747a5df9566db2d622fb16bf58f3454e682
Avsavi <shingavinash7@gmail.com>
2025-12-08 16:22:12 +0530

    code merge

--- a&03.js
+++ a&03.js
@@ -1,4 +1,5 @@
- // call object by using the function 
+/*
+// call object by using the function 
 
  let employee ={ ename:"avinash",ecode:123,edes:"developer",
     employeeContact:{ city:"pune",address:"123 aveneus pune"}
@@ -10,4 +11,33 @@ function getemployeeDetails(emp){
 }
 console.log(getemployeeDetails(employee));
 
+*/
+
+
+let obj= { cname:"Info", Cadd: "Pune,chakan",
+    cincome: 184000, temp:900
+}
+
+let b=obj;  // shalow copy 
+
+console.log(b);
+b.cname="ram";
+
+console.log(b)
+console.log(obj)
+
+
+let c = structuredClone(obj);    // deep copy 
+c.cname ="Techm";
+
+console.log(c);
+console.log(obj);
+
+
+let dcopyjson = JSON.parse(JSON.stringify(obj));
+
+dcopyjson.temp=7000;
+
+console.log(dcopyjson);
+console.log(obj);
 
