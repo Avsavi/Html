65390984fc8c8ac5a51a6c1307507da1274da647
Avsavi <shingavinash7@gmail.com>
2026-01-03 12:47:14 +0530

    code

---
+++ basicJS/controlstatements.js
@@ -0,0 +1,78 @@
+// control statements in JavaScript
+
+// if statement     
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
+
+// if...else statement
+let time = 14;
+if (time < 12) {
+    console.log("Good morning!");
+}
+else {
+    console.log("Good afternoon!");
+}
+
+// if...else if...else statement    
+let score = 31;
+if (score >= 60) {
+    console.log("Grade: A");
+}
+else if (score >= 45) {
+    console.log("Grade: B");
+}
+else if (score >= 35) {
+    console.log("Grade: C");
+}
+else {
+    console.log("Grade: F");
+}
+
+// switch statement
+
+// used for multiple conditions based on a single expression    
+let month = 33;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("March");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Invalid month");
+}
